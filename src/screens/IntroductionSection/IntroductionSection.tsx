6fbf1ceed9408fffa64575b72bdef35819830642
Tomas Leggire <tomasleggire@hotmail.com>
2024-03-07 17:04:48 -0300

    Efecto parrallax agregado

--- src/screens/IntroductionSection/IntroductionSection.tsx
+++ src/screens/IntroductionSection/IntroductionSection.tsx
@@ -2,16 +2,19 @@ import "./IntroductionSection.scss";
 
 export default function IntroductionSection() {
   return (
-    <div className="introduction-section" id="introduction-section">
-      <div className="introduction-section-header">
-        <h1>Carola Alegre</h1>
-        <h2>Fotografía Infantíl</h2>
-        <p>
-          Retrato Embarazadas, Bebés y Niños. Cada detalle importa a la hora de
-          capturar emociones, sentimientos y momentos únicos.
-        </p>
-        <button>Capturemos recuerdos que durarán toda la vida</button>
+    <>
+      <div className="background-container"></div>
+      <div className="introduction-section" id="introduction-section">
+        <div className="introduction-section-header">
+          <h1>Carola Alegre</h1>
+          <h2>Fotografía Infantíl</h2>
+          <p>
+            Retrato Embarazadas, Bebés y Niños. Cada detalle importa a la hora
+            de capturar emociones, sentimientos y momentos únicos.
+          </p>
+          <button>Capturemos recuerdos que durarán toda la vida</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
